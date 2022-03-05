db5a11aae4d65cac5e28e3c71ebf3135617f2714
aliramazon <mr.safarali@gmail.com>
2022-03-05 15:41:10 -0700

    Create API service for fetching data

---
+++ src/api/api.ts
@@ -0,0 +1,51 @@
+import {
+    generateFundsData,
+    generateCompanyData,
+    COMPANIES,
+    FUNDS,
+    EMAIL_WEBSITES,
+    HQ,
+    INDUSTRIES,
+    BUSINESS_MODELS,
+    FUNDING_ROUNDS,
+    Companies,
+    Funds
+} from '../mocks';
+
+const isDataAvailable = <T>(obj: T): boolean => {
+    return Object.keys(obj).length > 0;
+};
+
+class API {
+    companiesData: Companies;
+    fundsData: Funds;
+    constructor() {
+        this.companiesData = {};
+        this.fundsData = {};
+    }
+
+    fetchCompaniesData() {
+        if (isDataAvailable(this.companiesData)) return this.companiesData;
+        const companiesData = generateCompanyData(
+            COMPANIES,
+            EMAIL_WEBSITES,
+            HQ,
+            INDUSTRIES,
+            BUSINESS_MODELS,
+            FUNDING_ROUNDS
+        );
+        this.companiesData = companiesData;
+        return this.companiesData;
+    }
+
+    fetchFundsData() {
+        if (isDataAvailable(this.fundsData)) return this.fundsData;
+        if (!isDataAvailable(this.companiesData)) {
+            this.fetchCompaniesData();
+        }
+        this.fundsData = generateFundsData(FUNDS, this.companiesData);
+        return this.fundsData;
+    }
+}
+
+export default API;
