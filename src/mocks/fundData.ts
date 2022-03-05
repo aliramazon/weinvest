7174a092539fc35225ff41a326fe659d1624b780
aliramazon <mr.safarali@gmail.com>
2022-03-05 15:39:39 -0700

    Use Obj to store data using unique ID / nanoID

--- src/mocks/fundData.ts
+++ src/mocks/fundData.ts
@@ -1,22 +1,25 @@
-import { FUNDS, COMPANIES, Company, Fund, DIC } from './constants';
+import { nanoid } from 'nanoid';
+import { Companies, Fund, Funds, DIC } from './constants';
 import { pickRandomIdx } from './companyData';
 
-const generateFundsData = (FUNDS: string[], COMPANIES: Company[]) => {
-    const data = [];
+const generateFundsData = (FUNDS: string[], COMPANIES: Companies) => {
+    const companies = Object.values(COMPANIES);
+    const data: Funds = {};
+
     let startIdx = 0,
         endIdx = 10;
 
     for (let idx = 0; idx < FUNDS.length; idx++) {
         const fundName = FUNDS[idx];
+        const fundId = nanoid();
         const fund: Fund = {
             name: fundName,
-            id: idx,
+            id: fundId,
             investedIn: []
         };
 
-        let investedCompanies: number[] = [];
         for (let i = startIdx; i < endIdx; i++) {
-            const company = COMPANIES[i];
+            const company = companies[i];
             const companyFundingRounds = company.business.fundingRounds;
             let investedRoundIdx = pickRandomIdx(companyFundingRounds.length);
             const investedRoundName = DIC[investedRoundIdx];
@@ -38,16 +41,17 @@ const generateFundsData = (FUNDS: string[], COMPANIES: Company[]) => {
                     company.business.potentialExitValuation) /
                 100;
             const companyObj = {
-                multiple: impliedValue / investedAmount,
+                name: company.info.name,
                 id: company.id,
                 investedRound: investedRoundName,
                 investedAmount: investedAmount,
                 ownershipPercentage: ownershipPercentage,
-                impliedValue: impliedValue
+                impliedValue: impliedValue,
+                multiple: impliedValue / investedAmount
             };
             fund.investedIn.push(companyObj);
         }
-        data.push(fund);
+        data[fundId] = fund;
         startIdx += 10;
         endIdx += 10;
     }
