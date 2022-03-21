f79dbe2348fc6189eb6d03e1b051253f71d3da70
aliramazon <mr.safarali@gmail.com>
2022-03-20 22:51:12 -0600

    Create a type for each Action

--- src/store/actions/types.ts
+++ src/store/actions/types.ts
@@ -4,10 +4,23 @@ import { CompaniesActions } from './companies';
 export const Actions = { ...FundsActions, ...CompaniesActions };
 export type ActionsType = FundsActions | CompaniesActions;
 
-export type FetchFundsPayload = undefined;
-export type Payload = FetchFundsPayload;
-
-export interface ActionType {
-    type: ActionsType;
-    payload?: Payload;
+interface FetchFundsDataAction {
+    type: FundsActions.FETCH_FUNDS_DATA;
 }
+
+interface FetchCompaniesData {
+    type: CompaniesActions.FETCH_COMPANIES_DATA;
+}
+interface SortFundsInvestedIn {
+    type: FundsActions.SORT_FUNDS_INVESTEDIN;
+    payload: {
+        fundId: string;
+        columnIdx: string;
+        sortDirection: 'ASC' | 'DESC';
+    };
+}
+
+export type ActionType =
+    | FetchFundsDataAction
+    | SortFundsInvestedIn
+    | FetchCompaniesData;
--- src/store/reducers/funds.ts
+++ src/store/reducers/funds.ts
@@ -1,17 +1,30 @@
 import { FundsState } from '../state/state';
 import { ActionType, Actions } from '../actions';
 import API from '../../api/';
+import { FundedCompany } from '../../mocks';
 const api = new API();
 
 export const funds = (
-    state = {} as FundsState,
+    draft = {} as FundsState,
     action: ActionType
 ): FundsState => {
     switch (action.type) {
         case Actions.FETCH_FUNDS_DATA:
-            state.data = api.fetchFundsData();
-            return state;
+            draft.data = api.fetchFundsData();
+            return draft;
+        case Actions.SORT_FUNDS_INVESTEDIN:
+            const fund = draft.data[action.payload.fundId];
+            fund.investedIn.sort((a: FundedCompany, b: FundedCompany) => {
+                const columnIdx = action.payload
+                    .columnIdx as keyof FundedCompany;
+                if (action.payload.sortDirection === 'ASC') {
+                    return (a[columnIdx] as number) - (b[columnIdx] as number);
+                }
+                return (b[columnIdx] as number) - (a[columnIdx] as number);
+            });
+            draft.data[action.payload.fundId] = fund;
+            return draft;
         default:
-            return state;
+            return draft;
     }
 };
